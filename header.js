b0454d20ed57bd5c6d754bb1fce99fbb8a3f7db5
Muhammadbey <mahkamovmuhammadbey@gmail.com>
2023-01-14 21:45:26 +0500

    first commit

--- header.js
+++ header.js
@@ -4,3 +4,16 @@ const hamberger = document.getElementById("hamberger");
 hamberger.addEventListener("click", (e) => {
   card.classList.toggle("active-navbar");
 });
+
+const header = document.getElementById("header");
+const headerHeight = header.offsetHeight;
+
+const fixHeader = () => {
+  if (window.pageYOffset >= headerHeight) {
+    header.classList.add("sticky_header");
+  } else {
+    header.classList.remove("sticky_header");
+  }
+};
+
+window.addEventListener("scroll", fixHeader);
